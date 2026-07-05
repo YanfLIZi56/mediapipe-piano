71a4788c96ad65a1cc56a5ee6755a6f3dfc18ec0
YangLiZi56 <3795737683@qq.com>
2026-07-05 09:52:35 +0800

    修改流水线错误

--- scripts/download-mediapipe.ps1
+++ scripts/download-mediapipe.ps1
@@ -1,6 +1,4 @@
 $MP_VERSION = "0.4.1675469240"
-$CAM_VERSION = "0.3.1675466862"
-$DRAW_VERSION = "0.3.1675466124"
 $TARGET_DIR = "public/mediapipe"
 
 Write-Host "Downloading MediaPipe files to $TARGET_DIR..."
@@ -8,15 +6,15 @@ Write-Host "Downloading MediaPipe files to $TARGET_DIR..."
 New-Item -ItemType Directory -Force -Path $TARGET_DIR | Out-Null
 
 $files = @(
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands.js"; Name="hands.js"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands.binarypb"; Name="hands.binarypb"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_packed_assets.data"; Name="hands_solution_packed_assets.data"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_wasm_bin.js"; Name="hands_solution_wasm_bin.js"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_wasm_bin.wasm"; Name="hands_solution_wasm_bin.wasm"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_simd_wasm_bin.js"; Name="hands_solution_simd_wasm_bin.js"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_simd_wasm_bin.wasm"; Name="hands_solution_simd_wasm_bin.wasm"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils@$CAM_VERSION/camera_utils.js"; Name="camera_utils.js"},
-    @{Url="https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils@$DRAW_VERSION/drawing_utils.js"; Name="drawing_utils.js"}
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands.js"; Name = "hands.js" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands.binarypb"; Name = "hands.binarypb" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_packed_assets.data"; Name = "hands_solution_packed_assets.data" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_wasm_bin.js"; Name = "hands_solution_wasm_bin.js" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_wasm_bin.wasm"; Name = "hands_solution_wasm_bin.wasm" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_simd_wasm_bin.js"; Name = "hands_solution_simd_wasm_bin.js" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/hands@$MP_VERSION/hands_solution_simd_wasm_bin.wasm"; Name = "hands_solution_simd_wasm_bin.wasm" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"; Name = "camera_utils.js" },
+    @{Url = "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js"; Name = "drawing_utils.js" }
 )
 
 foreach ($file in $files) {
